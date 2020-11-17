ecd2370bf47ef131c8119814c2d46e64617082d1
thanhcong2005 <63457538+thanhcong2005@users.noreply.github.com>
2020-11-17 23:02:22 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*
+    Toan tu toan hoc: + - * / % ++ --
+    Toan tu so sanh: == != > >= < <=
+    Toan tu logic && || !
+    Toan tu gan: = += *= -= %=
+*
+int main()
+{
+    int a =100 ,b =200;
+    int c,d;
+    c =a/b;
+    c = ++a;
+    a = 100;
+    d = a++;
+    a || b;
+        printf("c = %d",c);
+        printf("c = %d",d);
+    return 0;
+}
+
